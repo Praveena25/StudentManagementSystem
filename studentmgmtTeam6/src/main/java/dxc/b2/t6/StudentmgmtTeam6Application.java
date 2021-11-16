d5ae37f5693df42d70c3acb86e9db3231118af9a
Your Name <praveenagovind25@gmail.com>
2021-11-16 02:06:49 -0800

    Student management System

---
+++ studentmgmtTeam6/src/main/java/dxc/b2/t6/StudentmgmtTeam6Application.java
@@ -0,0 +1,13 @@
+package dxc.b2.t6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmgmtTeam6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmgmtTeam6Application.class, args);
+	}
+
+}
